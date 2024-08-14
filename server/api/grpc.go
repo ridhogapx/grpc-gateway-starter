5c5243fa87f3343cd939dbaae6726a35d6617cf8
rneko26 <rneko2006@gmail.com>
2024-08-14 18:43:59 +0700

    feat: Add repository pattern in grpc stub

--- server/api/grpc.go
+++ server/api/grpc.go
@@ -1,13 +1,54 @@
 package api
 
 import (
+	"api-service/server/db/sql"
 	pb "api-service/server/proto"
+	"encoding/json"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type GRPCService struct {
+	repos *sql.Repository
 	pb.ApiServiceServer
 }
 
-func New() *GRPCService {
-	return &GRPCService{}
+func New(repos *sql.Repository) *GRPCService {
+	return &GRPCService{
+		repos: repos,
+	}
+}
+
+func (s *GRPCService) ValidatePayload(data interface{}, rules map[string]interface{}) []*pb.Error {
+
+	result := []*pb.Error{}
+
+	dataMap := map[string]interface{}{}
+
+	validate := validator.New()
+	dataBytes, _ := json.Marshal(data)
+
+	err := json.Unmarshal(dataBytes, &dataMap)
+	if err != nil {
+		return result
+	}
+
+	errs := validate.ValidateMap(dataMap, rules)
+	if len(errs) > 0 {
+
+		for k, v := range errs {
+			errValidator := v.(validator.ValidationErrors)
+
+			for _, el := range errValidator {
+				result = append(result, &pb.Error{
+					Field: k,
+					Tag:   el.ActualTag(),
+				})
+			}
+		}
+
+	}
+
+	return result
+
 }
