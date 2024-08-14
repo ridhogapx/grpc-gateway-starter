fc7bfab912976e004fd72df6a40a141f1dcdc0ad
rneko26 <rneko2006@gmail.com>
2024-08-14 18:44:17 +0700

    feat: Hash password before create execution

--- server/api/handler_authentication.go
+++ server/api/handler_authentication.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"api-service/server/db/model"
 	pb "api-service/server/proto"
 	"context"
 
+	"golang.org/x/crypto/bcrypt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -15,16 +17,48 @@ func (s *GRPCService) SignUp(ctx context.Context, req *pb.SignUpRequest) (*pb.Si
 		Message: "Sign-up is complete",
 	}
 
-	if req.GetEmail() == "" {
-		return nil, status.Errorf(codes.InvalidArgument, "Email field can't be empty.")
+	rulesValidator := map[string]interface{}{
+		"email":    "required,email,min=5,max=90",
+		"username": "required,min=4,max=50",
+		"name":     "required,max=50",
 	}
 
-	if req.GetUsername() == "" {
-		return nil, status.Errorf(codes.InvalidArgument, "Username can't be empty.")
+	errValidator := s.ValidatePayload(req, rulesValidator)
+	if len(errValidator) > 0 {
+
+		result.Code = codes.InvalidArgument.String()
+		result.Message = "Invalid input"
+		result.Errors = errValidator
+
 	}
 
-	if req.GetName() == "" {
-		return nil, status.Errorf(codes.InvalidArgument, "Name field can't be empty.")
+	validateUser, err := s.repos.FindUser(&model.User{Username: req.GetUsername()}, []string{"users.id", "users.username", "users.email"})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(validateUser) > 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "Username has been already taken. Please use other username!")
+	}
+
+	userORM := &model.User{}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.GetPassword()), 14)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Internal Error")
+	}
+
+	userORM.Username = req.GetUsername()
+	userORM.Name = req.GetName()
+	userORM.Email = req.GetEmail()
+	userORM.Password = string(hashedPassword)
+	userORM.ProfilePict = req.GetProfilePicture()
+	userORM.AllowNSFW = req.GetAllowNsfw()
+	userORM.Status = "PENDING"
+
+	_, err = s.repos.UpdateOrCreateUser(userORM)
+	if err != nil {
+		return nil, err
 	}
 
 	return result, nil
