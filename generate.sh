cc198b7e2c9f42184c5ce629bb40a2b7135ffcd2
root <rneko2006@gmail.com>
2024-09-10 17:53:35 +0700

    feat: Update protoc generator script

--- generate.sh
+++ generate.sh
@@ -6,3 +6,8 @@ protoc -I . \
 protoc -I . --grpc-gateway_out ./server/ \
     --grpc-gateway_opt paths=source_relative \
     proto/*.proto
+
+protoc --proto_path=./proto ./proto/orm.proto \
+    --proto_path=./ \
+    --plugin=$(go env GOPATH)/bin/protoc-gen-gorm \
+    --gorm_out=./server
